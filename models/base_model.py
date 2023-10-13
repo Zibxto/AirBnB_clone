543066be4a3d22ad31bf1f7faf8210e251865ea5
Abdul <aabdulmajeed.isa@gmail.com>
2023-10-13 17:11:03 +0100

    fix up base_model

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ Defines all common attributes/methods for other classes
 """
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
@@ -14,13 +14,12 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
 
         self.id = str(uuid.uuid4())
-        date_now = datetime.now()
-        self.created_at = date_now
-        self.updated_at = date_now
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
         if kwargs:
             for key in kwargs.keys():
-                if key == "__classname__":
+                if key == "__class__":
                     continue
                 elif key == "created_at":
                     self.created_at = datetime.strptime(kwargs[key],
@@ -30,30 +29,31 @@ class BaseModel:
                                                         "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     setattr(self, key, kwargs.get(key))
-        storage.new(self.to_dict())
+        else:
+            models.storage.new(self)
 
     def save(self):
         """
         Updates the public instance attribute updated_at with
         the current datetime
         """
-        storage.save()
         self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """
         returns a dictionary containing all keys/values
         """
-        curr_dict = self.__dict__
-        curr_dict['__classname__'] = __class__.__name__
-        str_fmt = "%Y-%m-%dT%H:%M:%S.%f"
-        curr_dict['created_at'] = curr_dict['created_at'].strftime(str_fmt)
-        curr_dict['updated_at'] = curr_dict['updated_at'].strftime(str_fmt)
+        curr_dict = self.__dict__.copy()
+        curr_dict['__class__'] = self.__class__.__name__
+        curr_dict['created_at'] = self.created_at.isoformat()
+        curr_dict['updated_at'] = self.updated_at.isoformat()
         return curr_dict
 
     def __str__(self):
         """
         String representation
         """
-        return "[{}] ({}) {}".format(__class__.__name__,
+        return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
