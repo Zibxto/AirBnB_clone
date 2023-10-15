cbdb48930a957bce0e0f8d0401360cd928138bb3
Abdul <aabdulmajeed.isa@gmail.com>
2023-10-15 16:41:09 +0100

    modify test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,36 +11,36 @@ class TestBaseModel(unittest.TestCase):
         id2 = BaseModel().id
         self.assertNotEqual(id1, id2)
 
-    def test_instance_uniqueness(self):
-        """
-        Test that instances are unique.
-        """
-        inst = BaseModel()
-        inst2 = BaseModel()
-        self.assertNotEqual(inst, inst2)
+    # def test_instance_uniqueness(self):
+    #     """
+    #     Test that instances are unique.
+    #     """
+    #     inst = BaseModel()
+    #     inst2 = BaseModel()
+    #     self.assertNotEqual(inst, inst2)
 
-    def test_id_is_string(self):
-        """
-        Test that the id is a string
-        """
-        id1 = BaseModel().id
-        self.assertIsInstance(id1, str)
+    # def test_id_is_string(self):
+    #     """
+    #     Test that the id is a string
+    #     """
+    #     id1 = BaseModel().id
+    #     self.assertIsInstance(id1, str)
 
-    def test_model_is_instance_class(self):
-        """
-        Test that the model is an instance of BaseModel
-        """
-        inst = BaseModel()
-        self.assertIsInstance(inst, BaseModel)
+    # def test_model_is_instance_class(self):
+    #     """
+    #     Test that the model is an instance of BaseModel
+    #     """
+    #     inst = BaseModel()
+    #     self.assertIsInstance(inst, BaseModel)
 
-    def test_instance_str(self):
-        """
-        Test that the instance print string representation
-        """
-        inst = BaseModel()
-        id1 = inst.id
-        string = "[{}] ({}) {}".format(inst.__class__.__name__, id1, inst.__dict__)
-        self.assertEqual(inst.__str__(), string)
+    # def test_instance_str(self):
+    #     """
+    #     Test that the instance print string representation
+    #     """
+    #     inst = BaseModel()
+    #     id1 = inst.id
+    #     string = "[{}] ({}) {}".format(inst.__class__.__name__, id1, inst.__dict__)
+    #     self.assertEqual(inst.__str__(), string)
 
 if __name__ == '__main__':
     unittest.main()
