c7f023c8086494273b254278883767cc1c81f04a
Abdul <aabdulmajeed.isa@gmail.com>
2023-10-15 16:33:40 +0100

    update BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -11,14 +11,10 @@ class BaseModel:
     """
     class for BaseModel
     """
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
         Initialization of BaseModel
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
         if kwargs:
             for key in kwargs.keys():
                 if key == "__class__":
@@ -32,6 +28,9 @@ class BaseModel:
                 else:
                     setattr(self, key, kwargs.get(key))
         else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def save(self):
