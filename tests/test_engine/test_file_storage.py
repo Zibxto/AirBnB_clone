5fc5b2d5c255699abc3bae8c1e8fea62b4b3d076
Abdul <aabdulmajeed.isa@gmail.com>
2023-10-15 16:58:29 +0100

    move test_engine folder

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -1,12 +1,12 @@
 import unittest
-from models.engine.file_storage import FileStorage
+from models import storage
 
 class TestFileStorage(unittest.TestCase):
     """Test cases for this model""" 
     def test_all(self):
         """ Ensure that default dictionary is empty """
-        obj = FileStorage().all()
-        self.assertEqual({}, {})
+        obj = storage.all()
+        self.assertEqual(obj, {})
 
 if __name__ == '__main__':
     unittest.main()
