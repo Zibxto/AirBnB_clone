bf0e2d819a95bc8ef249dc32446065186f28ea5a
Abdul <aabdulmajeed.isa@gmail.com>
2023-10-16 21:48:15 +0100

    add test cases for console

--- tests/test_console.py
+++ tests/test_console.py
@@ -12,5 +12,32 @@ class TestConsole(unittest.TestCase):
             output = f.getvalue().strip()
         self.assertEqual(output, "Quit command to exit the program")
 
+    def test_EOF(self):
+        """ Ensure that EOF is present"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help EOF")
+            output = f.getvalue().strip()
+        self.assertEqual(output, "Quit command to exit the program")
+
+    def test_empty_line(self):
+        """ Ensure that line is empty"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("")
+            output = f.getvalue().strip()
+        self.assertEqual(output, "")
+
+    def test_create_and_show_model(self):
+        """ Ensure that model is created"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+            id = f.getvalue().strip()
+        self.assertTrue(id)
+        
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show BaseModel {}".format(id))
+            output = f.getvalue().strip()
+        self.assertTrue(output)
+
+
 if __name__ == '__main__':
     unittest.main()
